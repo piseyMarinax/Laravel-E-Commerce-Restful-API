51abf1d74a59b65dd313cff20e75050eb73ab161
piseyMarinax <pisey@marinax.biz>
2018-04-22 11:28:46 +0700

    Product Delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -109,6 +109,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+         $product->delete();
+         return response('Successfull delete Content');
     }
 }
